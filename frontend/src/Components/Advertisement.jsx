fe8ace89b69a6dfede7ef3c630b3bfd361d8685a
Verosha Kriyanjala <veroshakriyanjala32@gmail.com>
2024-07-14 00:00:08 +0530

    Browse Car conponenet and Grid Done

--- frontend/src/Components/Advertisement.jsx
+++ frontend/src/Components/Advertisement.jsx
@@ -1,6 +1,30 @@
-import "./Advertisemnets.css";
+import React from "react";
+import "./components.css";
 import Background from "./Background";
 
-function Advertisement() {
-  return <Background></Background>;
+function Advertisement({
+  brand,
+  model,
+  year,
+  price,
+  location,
+  mileage,
+  fuel,
+  image,
+}) {
+  return (
+    <Background className="advertisement">
+      <img src={image} alt="Car" />
+      <div className="details">
+        <div className="car-name">{`${year} ${brand} ${model}`}</div>
+        <p className="car-features">{`${mileage} | ${fuel} | ${location}`}</p>
+        <div className="car-price">${price}</div>
+      </div>
+      <div className="w-full px-4 py-2">
+        <button className="cont-seller-but">Contact Seller</button>
+      </div>
+    </Background>
+  );
 }
+
+export default Advertisement;
--- frontend/src/Pages/BrowseCars.js
+++ frontend/src/Pages/BrowseCars.js
@@ -1,5 +1,9 @@
+import React from "react";
 import "./BrowseCar.css";
 import Background from "../Components/Background";
+import Advertisement from "../Components/Advertisement";
+import AdvertisementData from "../Data/AdvertisementData";
+import image from "../assets/car.png";
 
 function BrowseCars() {
   return (
@@ -13,11 +17,29 @@ function BrowseCars() {
             type="text"
             className="input-field"
             placeholder="Search Here"
-          ></input>
+          />
         </div>
-        <div></div>
+        <div className="grid-container">
+          {AdvertisementData.map((ad) => (
+            <Advertisement
+              key={ad.id}
+              brand={ad.brand}
+              model={ad.model}
+              year={ad.year}
+              price={ad.price}
+              location={ad.location}
+              mileage={ad.mileage}
+              fuel={ad.fuel}
+              image={image}
+            />
+          ))}
+        </div>
       </Background>
-      <Background className="filter"></Background>
+      <Background className="filter">
+        <div className="flex flex-row justify-start w-full p-1 h-dvh">
+          <span className="topic">Filters</span>
+        </div>
+      </Background>
     </div>
   );
 }
